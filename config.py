cbf55a31553f6019cc70cd5ea85701987d58f18a
Slawomir Lisowski <slisowski@gmail.com>
2023-05-01 08:03:21 +0200

    create repo

---
+++ config.py
@@ -0,0 +1,3 @@
+data_folder='/home/slawomir/Portfolio/algorithmic-trading/XtbClient/data/'
+spread_folder='/home/slawomir/Portfolio/algorithmic-trading/XtbClient/spreads/'
+data_folder_spreads='/home/slawomir/Portfolio/algorithmic-trading/XtbClient/data/spreads/'
